2053c70f48a1eab28a8abc20d168942581dda301
Sidong Charlie Feng <u6063820@anu.edu.au>
2019-08-09 21:30:24 +1000

    test dribbble

--- routes/screenshot.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('screenshot', {title: 'Mobile UI Gallery - Screenshot'});
-});
-
-module.exports = router;
