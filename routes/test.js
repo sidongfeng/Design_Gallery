beb9fd1085752facc4bb9a4060a586f80fc6bd37
Sidong Charlie Feng <u6063820@anu.edu.au>
2019-08-31 13:13:58 +1000

    add store query.txt

--- routes/test.js
+++ routes/test.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    const fs = require('fs') 
+    let query = req["query"]["text"]+'\n'
+    fs.appendFile('Query.txt', query, (err) => { 
+        // In case of a error throw err. 
+        if (err) throw err; 
+    }) 
+    console.log("query: "+query)
     res.render('test', {title: 'Mobile UI Gallery - Test'});
 });
 
