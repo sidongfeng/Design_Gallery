0508a96fd2049e4d726dd578c89ed917fc1255ae
Sidong Charlie Feng <u6063820@anu.edu.au>
2019-09-18 16:00:30 +1000

    add upload to find similar widgets

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,58 @@
 const express = require('express');
 const router = express.Router();
 //const async = require('async');
+const path = require('path');
+const multer = require('multer');
+const spawn = require("child_process").spawn;
 
-const mongoose = require('mongoose');
-const Widget = mongoose.model('Widget');
-const Util = require('../util/util');
+/**
+ * Run python script, pass in `-u` to not buffer console output 
+ * @return {ChildProcess}
+ */
+function runScript(){
+    return spawn('python', [
+      "-u", 
+      './public/test.py',
+      "--foo", "some value for foo",
+    ]);
+  }
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb){
+      cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+  });
+
+// Init Upload
+const upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000},
+    fileFilter: function(req, file, cb){
+      checkFileType(file, cb);
+    }
+  }).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb){
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = filetypes.test(file.mimetype);
+  
+    if(mimetype && extname){
+      return cb(null,true);
+    } else {
+      cb('Error: Images Only!');
+    }
+  }
+
+// const mongoose = require('mongoose');
+// const Widget = mongoose.model('Widget');
+// const Util = require('../util/util');
 
 const _btnTypeArr = ["All", "Button", "CheckBox", "Chronometer", "ImageButton", "ProgressBar", "RadioButton", "RatingBar", "SeekBar", "Spinner", "Switch", "ToggleButton"];
 //"EditText", "View"
@@ -39,96 +87,120 @@ router.get('/', function (req, res, next) {
     });
 });
 
-router.get('/:package/:screenshotID', function (req, res, next) {
-    let findObj = {};
-    findObj.package_name = req.params.package;
-    findObj.src = new RegExp(req.params.screenshotID);
-    Widget.find(findObj)
-        .exec(function (err, doc) {
-            if (err) {
-                return next(err);
-            }
-            res.render('screenshot', {
-                title: 'Mobile UI Gallery - Widgets on the same screenshot',
+router.post('/', function (req, res, next) {
+    upload(req, res, (err) => {
+        if(err){
+          // change ejs name
+          res.render('search', {
+            msg: err
+          });
+        } else {
+          if(req.file == undefined){
+            res.render('search', {
+                msg: 'Error: No File Selected!',
+                title: 'Mobile UI Gallery - Search for widgets',
                 url: req.originalUrl,
                 btnTypeArr: _btnTypeArr,
                 sortTypeDict: _sortTypeDict,
                 colArr: _colArr,
                 catArr: _catArr,
                 query: req.query,
-                widgets: doc
             });
-        });
-
+          } else {
+            const subprocess = runScript()
+            subprocess.stdout.on('data', (data) => {
+              console.log(`data:${data}`);
+            });
+            subprocess.stderr.on('data', (data) => {
+              console.log(`error:${data}`);
+            });
+            subprocess.stderr.on('close', () => {
+              console.log("Closed");
+            });
+            res.render('search', {
+                msg: 'File Uploaded!',
+                file: `${req.file.filename}`,
+                title: 'Mobile UI Gallery - Search for widgets',
+                url: req.originalUrl,
+                btnTypeArr: _btnTypeArr,
+                sortTypeDict: _sortTypeDict,
+                colArr: _colArr,
+                catArr: _catArr,
+                query: req.query,
+                sims: s,
+            },);
+          }
+        }
+    });
 });
 
-router.post('/', function (req, res, next) {
-    if (!Util.isPositiveInteger(req.body.page) && req.body.page) {
-        return next(new Error('Page is not a positive integer'));
-    } else {
-        let findObj = {};
-        if (req.body.btnType === 'All') {
-            findObj = {};
-        } else {
-            findObj.widget_class = new RegExp(req.body.btnType + '\\d?');
-            // findObj.widget_class = req.body.btnType;
-        }
-        if (req.body.color !== 'All') {
-            findObj.color = req.body.color;
-        }
-        if (req.body.category !== 'All') {
-            findObj.category = req.body.category;
-        }
-        if (req.body.text !== '') {
-            findObj.text = new RegExp(req.body.text);
-        }
-        // Checking and parsing of width and height
-        let _widthArr = req.body.width.split(';').slice(0, 2);
-        if (_widthArr.every(function (value) {
-            return (value >= 0 && value <= 800);
-        })) {
-            findObj['dimensions.width'] = {"$gte": _widthArr[0], "$lte": _widthArr[1]};
-        } else {
-            return next(new Error('Invalid width.'));
-        }
-        let _heightArr = req.body.height.split(';').slice(0, 2);
-        if (_heightArr.every(function (value) {
-            return (value >= 0 && value <= 1280);
-        })) {
-            findObj['dimensions.height'] = {"$gte": _heightArr[0], "$lte": _heightArr[1]};
-        } else {
-            return next(new Error('Invalid height.'));
-        }
+// router.post('/', function (req, res, next) {
+//     if (!Util.isPositiveInteger(req.body.page) && req.body.page) {
+//         return next(new Error('Page is not a positive integer'));
+//     } else {
+//         let findObj = {};
+//         if (req.body.btnType === 'All') {
+//             findObj = {};
+//         } else {
+//             findObj.widget_class = new RegExp(req.body.btnType + '\\d?');
+//             // findObj.widget_class = req.body.btnType;
+//         }
+//         if (req.body.color !== 'All') {
+//             findObj.color = req.body.color;
+//         }
+//         if (req.body.category !== 'All') {
+//             findObj.category = req.body.category;
+//         }
+//         if (req.body.text !== '') {
+//             findObj.text = new RegExp(req.body.text);
+//         }
+//         // Checking and parsing of width and height
+//         let _widthArr = req.body.width.split(';').slice(0, 2);
+//         if (_widthArr.every(function (value) {
+//             return (value >= 0 && value <= 800);
+//         })) {
+//             findObj['dimensions.width'] = {"$gte": _widthArr[0], "$lte": _widthArr[1]};
+//         } else {
+//             return next(new Error('Invalid width.'));
+//         }
+//         let _heightArr = req.body.height.split(';').slice(0, 2);
+//         if (_heightArr.every(function (value) {
+//             return (value >= 0 && value <= 1280);
+//         })) {
+//             findObj['dimensions.height'] = {"$gte": _heightArr[0], "$lte": _heightArr[1]};
+//         } else {
+//             return next(new Error('Invalid height.'));
+//         }
 
-        switch (req.body.sortType) {
-            case 'appDownloads':
-                _sortType = {
-                    downloads: -1,
-                    color: 1
-                };
-                break;
-            case 'appAlpbAsc':
-                _sortType = {
-                    application_name: 1,
-                    color: 1
-                };
-                break;
-            default:
-                break
-        }
+//         switch (req.body.sortType) {
+//             case 'appDownloads':
+//                 _sortType = {
+//                     downloads: -1,
+//                     color: 1
+//                 };
+//                 break;
+//             case 'appAlpbAsc':
+//                 _sortType = {
+//                     application_name: 1,
+//                     color: 1
+//                 };
+//                 break;
+//             default:
+//                 break
+//         }
 
-        Widget.find(findObj)
-            //.sort(_sortType)
-            .skip((req.body.page - 1) * displayPerPage)
-            .limit(displayPerPage)
-            .exec(function (err, doc) {
-                if (err) {
-                    return next(err);
-                }
-                res.json(doc);
-            });
-    }
+//         Widget.find(findObj)
+//             //.sort(_sortType)
+//             .skip((req.body.page - 1) * displayPerPage)
+//             .limit(displayPerPage)
+//             .exec(function (err, doc) {
+//                 if (err) {
+//                     return next(err);
+//                 }
+//                 res.json(doc);
+//             });
+//     }
 
-});
+// });
 
 module.exports = router;
